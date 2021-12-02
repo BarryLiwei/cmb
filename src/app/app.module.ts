8eccc6eab068297bb2c71b911f7b9eb66f9b2c1a
liweibin <506540724@qq.com>
2021-12-02 19:40:44 +0800

    antd引入ng-zorro-antd 8.5.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgZorroAntdModule
   ],
   providers: [],
   bootstrap: [AppComponent]
